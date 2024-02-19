6ac05ef3a9284066271d2223b86dc58a8e872c11
Abhinav Kumar <quic_abhikuma@quicinc.com>
2024-02-18 21:45:07 -0800

    qcacmn: Add QCA vendor attribute for BTM support configuration
    Add a vendor attribute to configure BTM support in STA mode.
    
    Change-Id: Icba0f008bc63b21d125f8ba685dc97af7c1b9e95
    CRs-Fixed: 3742264

--- os_if/linux/qca_vendor.h
+++ os_if/linux/qca_vendor.h
@@ -5374,6 +5374,14 @@ enum qca_wlan_vendor_attr_config {
 	 */
 	QCA_WLAN_VENDOR_ATTR_CONFIG_PEER_AMPDU_CNT = 103,
 
+	/* 8-bit unsigned value to configure BTM support.
+	 *
+	 * The attribute is applicable only for STA interface. Uses enum
+	 * qca_wlan_btm_support values. This configuration is not allowed in
+	 * connected state.
+	 */
+	QCA_WLAN_VENDOR_ATTR_CONFIG_BTM_SUPPORT = 107,
+
 	/* 16-bit unsigned value to configure client's keep-alive interval in
 	 * seconds. The driver will reduce the keep-alive interval to this
 	 * configured value if the AP advertises BSS maximum idle period and if
@@ -15611,5 +15619,25 @@ enum qca_wlan_vendor_attr_link_reconfig {
 	QCA_WLAN_VENDOR_ATTR_LINK_RECONFIG_AFTER_LAST,
 	QCA_WLAN_VENDOR_ATTR_LINK_RECONFIG_MAX =
 	QCA_WLAN_VENDOR_ATTR_LINK_RECONFIG_AFTER_LAST - 1,
+};
+
+/**
+ * enum qca_wlan_btm_support: BTM support configuration
+ *
+ * @QCA_WLAN_BTM_SUPPORT_DEFAULT: Restore default BTM support policy. The driver
+ * follows the BSS Transition bit in the Extended Capabilities element from the
+ * connect request IEs with the default BTM support policy.
+ *
+ * @QCA_WLAN_BTM_SUPPORT_DISABLE: Disable BTM support for the subsequent
+ * (re)association attempts. The driver shall restore the default BTM support
+ * policy during the first disconnection after successful association. When this
+ * configuration is enabled, the driver shall overwrite the BSS Transition bit
+ * as zero in the Extended Capabilities element while sending (Re)Association
+ * Request frames. Also, the driver shall drop the BTM frames from userspace and
+ * the connected AP when this configuration is enabled.
+ */
+enum qca_wlan_btm_support {
+	QCA_WLAN_BTM_SUPPORT_DEFAULT = 0,
+	QCA_WLAN_BTM_SUPPORT_DISABLE = 1,
 };
 #endif
